6d56205de9719897fe70d4da5bcc45420b96468b
Malgon33 <Malgon33@2745e0a8-15af-11df-9a75-2f9892f9ffe9>
2010-02-10 14:35:52 +0000

    changing name in com.malgon.applecrunch

---
+++ trunk/src/com/malgon/applecrunch/Settings.java
@@ -0,0 +1,34 @@
+/*
+ * 
+	This file is part of Apple Crunch.
+
+    Apple Crunch is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Apple Crunch is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Apple Crunch.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.malgon.applecrunch;
+ 
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+ 
+public class Settings extends PreferenceActivity {
+ 
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+ 
+        addPreferencesFromResource(R.xml.settings);
+    }
+}
